4b46dcf596956dc862f7cf96fca2d68be70a59ac
Victoria <niaba.victoria@gmail.com>
2020-12-16 16:41:00 +0100

    Feat(Edit Person): masquage du bouton d'édition d'infos. personnelles

--- projects/gestion-de-cv/src/gestioncv/web/CvView.java
+++ projects/gestion-de-cv/src/gestioncv/web/CvView.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -28,6 +30,7 @@ public class CvView implements Serializable {
 	/* Donn�es utilis�es dans les vues */
 	private @Getter Collection<Person> persons;
 	private @Getter Person selectedPerson;
+	private @Getter boolean showEditButtons;
 
 	public Collection<Activity> getSelectedPersonEducations() {
 		return filterActivitiesByNature(selectedPerson, Nature.EDUCATION);
@@ -48,7 +51,7 @@ public class CvView implements Serializable {
 	private Collection<Activity> filterActivitiesByNature(Person person, Nature nature) {
 		if (person != null) {
 			return person.getCv().stream()
-					.filter(activity -> activity.getNature().equals(nature))
+					.filter(activity -> nature.equals(activity.getNature()))
 					.collect(Collectors.toList());
 		}
 
@@ -68,7 +71,7 @@ public class CvView implements Serializable {
 	 * @return
 	 */
 	public String show(int id) {
-		selectedPerson = personDao.find(id);
+		updateSelectedPerson(personDao.find(id));
 		return "cv-detail?faces-redirect=true";
 	}
 
@@ -78,11 +81,33 @@ public class CvView implements Serializable {
 
 	public String savePerson(Person person) {
 		if (person.getId() != 0) {
-			selectedPerson = personDao.update(person);
+			updateSelectedPerson(personDao.update(person));
 		} else {
-			selectedPerson = personDao.add(person);
+			updateSelectedPerson(personDao.add(person));
 		}
 		// TODO: faire en sorte que la mise-�-jour soit visible au sein de la collection
 		return "cv-detail";
+	}
+
+	/**
+	 * La modification de selectedPerson doit être effectuée uniquement à l'aide de
+	 * cette méthode afin de pouvoir appliquer des effets secondaires comme, par
+	 * exemple, dicter l'affichage des boutons de modifications de CVs.
+	 * 
+	 * @param newSelectedPerson
+	 */
+	private void updateSelectedPerson(Person newSelectedPerson) {
+		selectedPerson = newSelectedPerson;
+
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		Person loggedInUser = (Person) externalContext.getSessionMap().get("user");
+
+		// Effets secondaires de la mise à jour de selectedPerson
+		if (selectedPerson != null && loggedInUser != null && selectedPerson.getId() == loggedInUser.getId()) {
+			showEditButtons = true;
+		} else {
+			showEditButtons = false;
+		}
 	}
 }
